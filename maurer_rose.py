aa03a016bb352d21bb2e7c76bed69270fb5e6e91
wurtm <wurt.michal@gmail.com>
2026-01-09 13:15:18 +0100

    Maurer Rose program.

--- maurer_rose.py
+++ maurer_rose.py
@@ -1,5 +1,14 @@
 from svg_turtle import SvgTurtle
+import numpy as np
 
-t = SvgTurtle(500,500)
-t.forward(100)
-t.save_as("duck.svg")
+t = SvgTurtle(700,700)
+n = 3
+d = 19
+for angle in range(361):
+      t.color("red")
+      angle_rad = angle+1 * d * np.pi/180
+      r = 300 * np.sin(n * angle_rad)
+      x = r * np.sin(angle+1 * angle_rad)
+      y = r * np.cos(angle+1 * angle_rad)
+      t.goto(x,y)
+t.save_as("rose.svg")
